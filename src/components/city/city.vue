69b345e7b4a674bcc1892bb5d2e88c4d1257b5c7
947291135 <947291135@qq.com>
2018-11-19 11:23:32 +0800

    ESlist 格式替换

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -14,44 +14,42 @@ import CitySearch from './components/Search.vue'
 import CityList from './components/List.vue'
 import CityAlphabet from './components/Alphabet.vue'
 export default {
-    name:'City',
-    components: {
-        CityHeader,
-        CitySearch,
-        CityList,
-        CityAlphabet
-    },
-    data () {
-        return {
-            cities:{},//存储字母对应全部城市数据
-            hotCities:[],//存储热门城市
-            letter:''
-        }
-    },
-    methods: {
-        getCityInfo:function(){
-            let _this = this;
-            axios.get('http://api.mgblog.cn/qunaer/city.php').then(function(res){
-                res = res.data;
-                if(res.ret && res.data){
-                    const data =res.data;
-                    _this.cities =data.cities;
-                    _this.hotCities=data.hotCities;
-                    
-                }
-            })
-        },
-        handleLetterChange:function(letter){
-            this.letter=letter;
-        }
-    },
-    mounted () {
-        this.getCityInfo()//获取城市数据事件：get city
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data () {
+    return {
+      cities: {}, // 存储字母对应全部城市数据
+      hotCities: [], // 存储热门城市
+      letter: ''
     }
+  },
+  methods: {
+    getCityInfo: function () {
+      let _this = this
+      axios.get('http://api.mgblog.cn/qunaer/city.php').then(function (res) {
+        res = res.data
+        if (res.ret && res.data) {
+          const data = res.data
+          _this.cities = data.cities
+          _this.hotCities = data.hotCities
+        }
+      })
+    },
+    handleLetterChange: function (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()// 获取城市数据事件：get city
+  }
 
 }
 </script>
 
 <style lang="stylus" scoped>
-    
 </style>
